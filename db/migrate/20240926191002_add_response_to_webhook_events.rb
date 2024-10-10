f211641ab285b8f423c0369ff0dc56e38cc6aaa1
Raquel Fonsec <raque-leto@hotmail.com>
2024-10-10 15:43:35 +0200

    Fix migration to check if response column exists before adding

--- db/migrate/20240926191002_add_response_to_webhook_events.rb
+++ db/migrate/20240926191002_add_response_to_webhook_events.rb
@@ -1,5 +1,7 @@
 class AddResponseToWebhookEvents < ActiveRecord::Migration[7.1]
   def change
-    add_column :webhook_events, :response, :jsonb, default: {}
+    unless column_exists?(:webhook_events, :response)
+      add_column :webhook_events, :response, :jsonb, default: {}
+    end
   end
 end
